560cbac4969263a945693dcd22fb03582ffd470d
olivia l <olivialu415@gmail.com>
2025-11-20 09:52:05 -0500

    ews

--- test2.py
+++ test2.py
@@ -1,4 +1,26 @@
-x = "aaaaaaaaaaaaaaaaa"
-if any(char.isupper() for char in x) == False:
-        print("Please add an uppercase letter")
+import tkinter as tk
 
+class MyCustomWidget(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label = tk.Label(self, text="Hello from a custom widget!")
+        self.label.pack()
+
+        self.button = tk.Button(self, text="Click Me", command=self.on_button_click)
+        self.button.pack()
+
+    def on_button_click(self):
+        self.label.config(text="Button clicked!")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Class-Based Tkinter")
+
+    custom_widget = MyCustomWidget(root)
+    custom_widget.pack(pady=20)
+
+    root.mainloop()
