4e9670d2ab649e6fbf9d267d33f2c7cdc177b407
olivia-l0803 <olivialu415@gmail.com>
2025-12-12 15:30:50 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
-import PIL
-name = input("What is your name ")
+
+name = input("What is your name? ")
+name = name.strip()
+name = name.title()
 
 print("You are smelly, " + name)
 print("You are smelly," , name)
@@ -12,32 +13,5 @@ while "yes" not in x:
     x = input("Are you bald? ")
 
 
-print("hahahahahahhhhhahhahhahahhaahhahhahhahhahhahhhhaahhaa")
-=======
-import tkinter as tk
->>>>>>> 7e8847f1456e4c49fe2dba5ee8da0bb475cdbbe3
+print("hahahahahahhhhhahhahhahahhaahhahhahhahhahhahhhhaahhaa die" )
 
-class MyCustomWidget(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.label = tk.Label(self, text="Hello from a custom widget!")
-        self.label.pack()
-
-        self.button = tk.Button(self, text="Click Me", command=self.on_button_click)
-        self.button.pack()
-
-    def on_button_click(self):
-        self.label.config(text="Button clicked!")
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("Class-Based Tkinter")
-
-    custom_widget = MyCustomWidget(root)
-    custom_widget.pack(pady=20)
-
-    root.mainloop()
